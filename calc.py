270094d66eadfbb1efe4e369a9f7b57cb75cabf1
patricia  V <patriciav@Patricia-mac-book.local>
2026-01-03 16:55:24 +0530

    Initial commit: Basic Python calculator

---
+++ calc.py
@@ -0,0 +1,37 @@
+print("🧮 Welcome to Python Calculator")
+
+while True:
+    print("\nSelect operation:")
+    print("1. Addition (+)")
+    print("2. Subtraction (-)")
+    print("3. Multiplication (*)")
+    print("4. Division (/)")
+    print("5. Exit")
+
+    choice = input("Enter choice (1/2/3/4/5): ")
+
+    if choice == "5":
+        print("Calculator closed 👋")
+        break
+
+    if choice not in ["1", "2", "3", "4"]:
+        print("❌ Invalid choice. Try again.")
+        continue
+
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+
+    if choice == "1":
+        print("Result:", num1 + num2)
+
+    elif choice == "2":
+        print("Result:", num1 - num2)
+
+    elif choice == "3":
+        print("Result:", num1 * num2)
+
+    elif choice == "4":
+        if num2 == 0:
+            print("❌ Cannot divide by zero")
+        else:
+            print("Result:", num1 / num2)
